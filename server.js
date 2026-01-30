73cde051cd307265a60afefc77548bb1429e5c8e
AzharSabbir <azharsabbir25@gmail.com>
2026-01-30 16:07:28 +0600

    File uploading Feature added

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const port = 3000;
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(express.static(__dirname)); // Serves index.html
 
 // Database Connection
